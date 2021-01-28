c045df9400efe0808364394f51a35f687500b2ec
lolieatapple <lolieatapple@gmail.com>
2021-01-28 21:37:58 +0800

    update url

--- src/providers/connectors/wanwallet.ts
+++ src/providers/connectors/wanwallet.ts
@@ -6,6 +6,7 @@ const ConnectToWanWallet = async (
     await wanwallet.activate();
     provider = await wanwallet.getProvider();
   } else {
+    window.open('https://wanchain.org/getstarted');
     throw new Error("No Wan Wallet Provider found");
   }
   return provider;
